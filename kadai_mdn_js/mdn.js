6aaa9777d6eca687983ed2612c68ffeed425e734
yukiminoly <52592009+yukiminoly@users.noreply.github.com>
2025-06-15 03:31:44 +0900

    月取得処理を修正

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,6 +1,6 @@
 const data = new Date();
 const year = data.getFullYear();
-const month = data.getMonth();
+const month = data.getMonth() + 1;
 const day = data.getDate();
 
 console.log(`${year}年${month}月${day}日`);
